7f655622981098c4c727ced42f6abfafc7421a01
onndes <vladvyljotnikov@gmail.com>
2021-08-09 10:48:54 +0300

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import style from "./App.module.scss";
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className={style.app}>
+               
+            </div>
+        );
+    }
+}
