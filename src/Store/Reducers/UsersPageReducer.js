723c45822d8fe991a374968ff55dbcfd7c234c60
onndes <vladvyljotnikov@gmail.com>
2021-08-28 14:50:07 +0300

    component UserPage changed on class component

--- src/Store/Reducers/UsersPageReducer.js
+++ src/Store/Reducers/UsersPageReducer.js
@@ -3,38 +3,7 @@ const UN_FOLLOW = "UN-FOLLOW";
 const SET_USERS = "SET-USERS";
 
 const initialState = {
-    users: [
-        {
-            id: 1,
-            firstName: "Anna",
-            secondName: "Gorils",
-            photoUrl: "https://dekatop.com/wp-content/uploads/2019/01/people_03.jpg",
-            age: 22,
-            location: { city: "Kiev", country: "Ukraine" },
-            follow: true,
-            status: false,
-        },
-        {
-            id: 2,
-            firstName: "Viktor",
-            secondName: "Botr",
-            photoUrl: "https://www.liga.net/images/general/2019/02/14/20190214174619-9721.png",
-            age: 32,
-            location: { city: "Mosscow", country: "Russia" },
-            follow: false,
-            status: false,
-        },
-        {
-            id: 3,
-            firstName: "Kira",
-            secondName: "Morzi",
-            photoUrl: "https://yablyk.com/wp-content/uploads/2018/03/shakira.jpg",
-            age: 45,
-            location: { city: "Odessa", country: "Ukraine" },
-            follow: true,
-            status: true,
-        },
-    ],
+    users: [],
 };
 
 const UsersPageReducer = (state = initialState, action) => {
--- src/components/UsersPage/UsersPageOld.js
+++ src/components/UsersPage/UsersPageOld.js
@@ -3,17 +3,50 @@ import style from "./UsersPage.module.css";
 import * as axios from "axios";
 import userImg from "../../assets/img/iconUser.png";
 
-class UsersPage extends React.Component {
-    constructor(props) {
-        super(props);
+const UsersPage = (props) => {
+    const users = [
+        {
+            id: 1,
+            firstName: "Anna",
+            secondName: "Gorils",
+            photoUrl: "https://dekatop.com/wp-content/uploads/2019/01/people_03.jpg",
+            age: 22,
+            location: { city: "Kiev", country: "Ukraine" },
+            follow: true,
+            status: false,
+        },
+        {
+            id: 2,
+            firstName: "Viktor",
+            secondName: "Botr",
+            photoUrl: "https://www.liga.net/images/general/2019/02/14/20190214174619-9721.png",
+            age: 32,
+            location: { city: "Mosscow", country: "Russia" },
+            follow: false,
+            status: false,
+        },
+        {
+            id: 3,
+            firstName: "Kira",
+            secondName: "Morzi",
+            photoUrl: "https://yablyk.com/wp-content/uploads/2018/03/shakira.jpg",
+            age: 45,
+            location: { city: "Odessa", country: "Ukraine" },
+            follow: true,
+            status: true,
+        },
+    ];
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
-            this.props.setUsers(response.data.items);
-        });
-    }
-
-    renderUsersList = () => {
-        return this.props.usersPage.users.map((user) => {
+    const getUsers = () => {
+        if (props.usersPage.users.length === 0) {
+            // props.setUsers(users);
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
+                props.setUsers(response.data.items);
+            });
+        }
+    };
+    const renderUsersList = () => {
+        return props.usersPage.users.map((user) => {
             return (
                 <div key={user.id} className={style.userWrapp}>
                     <div className={style.firstCol}>
@@ -27,13 +60,13 @@ class UsersPage extends React.Component {
                             {user.follow ? (
                                 <button
                                     className={style.unfollow + " " + style.btnfollow}
-                                    onClick={() => this.props.unFollow(user.id)}>
+                                    onClick={() => props.unFollow(user.id)}>
                                     Unfollow
                                 </button>
                             ) : (
                                 <button
                                     className={style.follow + " " + style.btnfollow}
-                                    onClick={() => this.props.follow(user.id)}>
+                                    onClick={() => props.follow(user.id)}>
                                     Follow
                                 </button>
                             )}
@@ -59,16 +92,12 @@ class UsersPage extends React.Component {
             );
         });
     };
-    render() {
-        return (
-            <div className={style.Wrapper}>
-                {/* <button className={style.testBtn} onClick={getUsers}>
-                    [test] Get user
-                </button> */}
-                {this.renderUsersList()}
-            </div>
-        );
-    }
-}
+    return (
+        <div className={style.Wrapper}>
+            <button className={style.testBtn} onClick={getUsers}>[test] Get user</button>
+            {renderUsersList()}
+        </div>
+    );
+};
 
 export default UsersPage;
