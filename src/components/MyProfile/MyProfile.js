0bee48de4239750ac6121d8d7c4ceb5ffbe9e3ea
onndes <vladvyljotnikov@gmail.com>
2021-08-31 15:26:05 +0300

    add content on MyProfile

---
+++ src/components/MyProfile/MyProfile.js
@@ -0,0 +1,67 @@
+import React from "react";
+import style from "./MyProfile.module.css";
+import myfoto from "../../assets/img/myfoto.jpg";
+import userImg from "../../assets/img/iconUser.png";
+
+const HomePage = () => {
+    const myData = {
+        name: "Vlad Vylotnikov",
+        id: 19397,
+        uniqueUrlName: null,
+        photos: {
+            small: myfoto,
+            large: null,
+        },
+        status: true,
+        followed: false,
+    };
+    return (
+        <div>
+            <div key={myData.id} className={style.userWrapp}>
+                <div className={style.firstCol}>
+                    <div className={style.imgContainer}>
+                        <img
+                            src={myData.photos.small !== null ? myData.photos.small : userImg}
+                            alt=''
+                        />
+                    </div>
+                    <div className={style.btnContainer}>
+                        {myData.follow ? (
+                            <button className={style.unfollow + " " + style.btnFollow}>
+                                Unfollow
+                            </button>
+                        ) : (
+                            <button className={style.follow + " " + style.btnFollow}>Follow</button>
+                        )}
+                    </div>
+                </div>
+                <div className={style.secondCol}>
+                    <div className={style.nameAndOnlineBox}>
+                        <p className={style.titleName}>
+                            {`${myData.name} ${myData.secondName ? myData.secondName : ""}`}
+                        </p>
+                        {myData.status ? (
+                            <p className={style.statusOnline + " " + style.status}>Online</p>
+                        ) : (
+                            <p className={style.statusOffline + " " + style.status}>Offline</p>
+                        )}
+                    </div>
+                    <p className={style.titleLocation}>{"[hardcode]"}</p>
+                    <div className={style.aboutUserWrapper}>
+                        <ul className={style.aboutUserListTitle}>
+                            <li className={style.aboutUserItemTitle}>Birthday:</li>
+                            <li className={style.aboutUserItemTitle}>Town:</li>
+                            <li className={style.aboutUserItemTitle}>Photos:</li>
+                        </ul>
+                        <ul className={style.aboutUserList}>
+                            <li className={style.aboutUserItem}>[hardcode]</li>
+                            <li className={style.aboutUserItem}>[hardcode]</li>
+                            <li className={style.aboutUserItem}>[hardcode]</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default HomePage;
