5db167d48996fde1c824d2929240ac8fe6542129
onndes <vladvyljotnikov@gmail.com>
2021-09-01 12:53:32 +0300

    create component preloader

---
+++ src/Common/Preloader/Preloader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import preloader from "../../assets/img/preloader.svg";
+
+const Preloader = () => {
+    return <img src={preloader} alt='' />;
+};
+
+export default Preloader;
