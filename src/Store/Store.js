b7ebd5ab804a851726e79f7de805d95dc047e2b1
onndes <vladvyljotnikov@gmail.com>
2021-09-02 15:43:25 +0300

    create AuthReducer

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -3,12 +3,14 @@ import MessagesPageReducer from "./Reducers/MessagesPageReducer";
 import NewsPageReducer from "./Reducers/NewsPageReducer";
 import UsersPageReducer from "./Reducers/UsersPageReducer";
 import ProfilePageReducer from "./Reducers/ProfilePageReducer";
+import AuthReducer from "./Reducers/AuthReducer";
 
 let reducers = combineReducers({
     messagesPage: MessagesPageReducer,
     newsPage: NewsPageReducer,
     usersPage: UsersPageReducer,
     profilePage: ProfilePageReducer,
+    auth: AuthReducer,
 });
 
 let store = createStore(reducers);
