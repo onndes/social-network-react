951305978050f203522f21960547c627e577b02f
onndes <vladvyljotnikov@gmail.com>
2021-09-15 11:51:32 +0300

    improved App Preloader

--- src/Common/AppPreloader/AppPreloader.js
+++ src/Common/AppPreloader/AppPreloader.js
@@ -1,14 +1,18 @@
 import React from "react";
 import appPreloader from "../../assets/img/app_preloader.svg";
-import s from "./AppPreloader.module.css"
+import appPreloaderDots from "../../assets/img/app_preloared_dots.svg";
+import s from "./AppPreloader.module.css";
 
 const AppPreloader = (props) => {
     return (
         <>
-            <div className={s.wrapper}>
-                <img className={s.svg} src={appPreloader} alt='' />
-                <p className={s.text}>RR Loading...</p>
-            </div>
+              <div className={s.wrapper}>
+                  <img className={s.svg} src={appPreloader} alt='' />
+                  <div className={s.textWrapp}>
+                      <p className={s.text}>RR Loading</p>
+                      <img className={s.svgDots} src={appPreloaderDots} alt='' />
+                  </div>
+              </div>
         </>
     );
 };
