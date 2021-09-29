a157113dd1e435c4bddbc8aa0c61ea54900b01cb
onndes <vladvyljotnikov@gmail.com>
2021-09-29 12:38:19 +0300

    created component ProfileInfo

---
+++ src/components/ProfilePage/ProfileInfo/ProfileInfo.js
@@ -0,0 +1,41 @@
+import s from "./ProfileInfo.module.css";
+import React from "react";
+
+const ProfileInfo = ({ profile }) => {
+    const contacts = Object.keys(profile.contacts).map((title) => {
+        return (
+            <div key={title} className={s.wrapTitleInfoPI}>
+                <p className={s.titlePI}>{title}: </p>
+                <p className={s.descriptionPI}>{profile["title"] || "no data"}</p>
+            </div>
+        );
+    });
+
+    return (
+        <div className={s.wrappProfileInfoPI}>
+            {/* <div className={s.wrapTitleInfoPI}>
+              <p className={s.titlePI}>Full name: </p>
+              <p className={s.descriptionPI}>{profile.fullName}</p>
+          </div> */}
+            <div className={s.wrapTitleInfoPI}>
+                <p className={s.titlePI}>About me: </p>
+                <p className={s.descriptionPI}>{profile.aboutMe || "no data"}</p>
+            </div>
+            <div className={s.wrapTitleInfoPI}>
+                <p className={s.titlePI}>Looking for a job: </p>
+                <p className={s.descriptionPI}>{profile.lookingForAJob ? "yes" : "no"}</p>
+            </div>
+            <div className={s.wrapTitleInfoPI}>
+                <p className={s.titlePI}>What work I'm looking for: </p>
+                <p className={s.descriptionPI}>{profile.lookingForAJobDescription || "no data"}</p>
+            </div>
+            {/* Contacts block start */}
+            <div className={s.wrapTitleInfoPI}>
+                <p className={s.titlePI}>Contacts: </p>
+            </div>
+            <div className={s.wrapTitleInfoPI_contacts}>{contacts}</div>
+            {/* Contacts block end */}
+        </div>
+    );
+};
+export default ProfileInfo;
