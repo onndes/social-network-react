9b5da55bbdcf9a88b73b462d586c126d11af31fe
onndes <vladvyljotnikov@gmail.com>
2021-11-24 08:02:38 +0200

    TS: typing: LoginPage

--- src/components/LoginPage/LoginPageContainer.tsx
+++ src/components/LoginPage/LoginPageContainer.tsx
@@ -1,14 +1,27 @@
+import { AppStateType } from './../../Store/Store';
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import LoginPage from "./LoginPage";
 import { loginMe } from "../../Store/Reducers/AuthReducer";
 
-const LoginContainer = (props) => {
+type MapPropsType = {
+  isAuth:boolean
+  isLoading: boolean
+  captchaUrl: string | null
+}
+
+type MapDispatchType = {
+  loginMe: () => void
+}
+
+type PropsType = MapPropsType & MapDispatchType 
+
+const LoginContainer: React.FC<PropsType | any> = (props) => {
     return <LoginPage {...props} />;
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state: AppStateType): MapPropsType => {
     return {
         isAuth: state.auth.isAuth,
         isLoading: state.auth.isLoading,
